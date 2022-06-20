c1c05a901cf05a5a6d0ddb2e76915c2f313232e1
UJJWAL JOSHI <103943864+Ujjwal1110@users.noreply.github.com>
2022-06-20 09:12:16 +0530

    Add files via upload

---
+++ fetch.test.js
@@ -0,0 +1,10 @@
+
+import {getData} from '../script/appendFetch';
+
+test('testing fetching of Data', () => {
+
+    expect(getData('https://fakestoreapi.com/products/category/electronics')).toBe()
+    
+
+
+});
